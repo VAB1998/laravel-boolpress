5ceeac34ff29b48301477ed6daabc3bea7b95433
VAB1998 <blanco.vincenzo@outlook.com>
2021-11-15 16:26:55 +0100

    Posts Seeder Implemented

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Models\Post;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,8 +12,17 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i < 5; $i++){
+            
+            $newPost = new Post();
+            $newPost->title = $faker->sentence(4);
+            $newPost->author = $faker->name();
+            $newPost->post_date = $faker->dateTime();
+            $newPost->post_content = $faker->paragraphs(6, true);
+            $newPost->slug = Str::slug($newPost->title, '-');
+            $newPost->save();
+        }
     }
 }
