d138c371b904f89e958e53f8b29f33d92992b557
VAB1998 <blanco.vincenzo@outlook.com>
2021-11-21 14:18:16 +0100

    Implemented new functionality: visualization of the Tags in the Show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,6 +6,9 @@
             <a href="{{ route('admin.posts.index') }}">Go Back to the Index</a>
             <h1> {{ $post->title }} </h1>
             <h5>{{ $post->author }} | {{ $post->post_date }}</h5>
+            @foreach ($post->tags as $tag)
+                <span class="badge" style="background-color: {{ $tag->color}} ">{{$tag->name}}</span>
+            @endforeach
             <p> {{ $post->post_content }} </p>
         </div>
     </section>
